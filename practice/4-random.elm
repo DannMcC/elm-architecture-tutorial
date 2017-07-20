aec1c2b4458d486141a48f6f286ce6e6ea0c11c9
Dan McCune <dmccune@hivelocity.net>
2017-07-20 12:46:31 -0400

    hello RND

---
+++ practice/4-random.elm
@@ -0,0 +1,54 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, button, text, program)
+import Html.Events exposing (onClick)
+import Random
+
+{- Model
+-}
+type alias Model =
+    Int
+
+init : ( Model, Cmd Msg )
+init =
+    ( 1, Cmd.none )
+
+{- Messages
+-}
+type Msg =
+    Roll
+    | OnResult Int
+
+{- View
+-}
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Roll ] [ text "Roll" ]
+        , text (toString model)
+        ]
+
+{- Update
+-}
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Roll ->
+            ( model, Random.generate OnResult (Random.int 1 6) )
+
+        OnResult res ->
+            ( res, Cmd.none )
+{- Main
+-}
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (always Sub.none)
+        }
